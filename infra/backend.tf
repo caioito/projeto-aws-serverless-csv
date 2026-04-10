360e5c02c2c36a2e962da53d3563ca5d2da7840f
Caio Ito <caioito14@gmail.com>
2026-04-10 10:52:57 -0300

    fix: terraform backend and workflow

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,3 +1,12 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    # Nome do bucket S3 que vai armazenar o state
+    bucket = "meu-backend-terraform"
+
+    # Caminho do arquivo state dentro do bucket
+    key = "state/terraform.tfstate"
+
+    # Região AWS do bucket
+    region = "us-east-1"
+  }
 }
